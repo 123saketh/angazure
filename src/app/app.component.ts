44aa60aac665f1727719f9d7b83199e4bbb1bec6
sakethd <sakethd@zenoti.com>
2021-01-05 17:26:31 +0530

    comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare const test:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angazure';
+  constructor(){
+    //test();
+  }
 }
